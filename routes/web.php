ccbd3b15a6a019e3ab883b06b5f6eb4be691dd08
maxime_dujardin1 <maxime_dujardin1@ens.univ-artois.fr>
2020-11-12 12:43:09 +0100

    ajout route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/home', function () {
@@ -38,7 +38,7 @@ Route::get('/bureau_virtuel', function () {
 
 Route::get('/documentation',function(){
     return view('ent/documentation');
-});
+});*/
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function() {
 
