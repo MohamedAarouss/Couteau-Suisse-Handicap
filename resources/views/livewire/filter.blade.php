79ff62a1b8ef380321cc7e6abf814160467de5c8
Quentin <quentin_macq@ens.univ-artois.fr>
2020-11-24 17:42:24 +0100

    modifs

--- resources/views/livewire/filter.blade.php
+++ resources/views/livewire/filter.blade.php
@@ -1,8 +1,11 @@
 <div>
-    <br>
+    <label>
+        <input type="text" wire:model="searchTerm" placeholder="Rechercher..." class="form-control m-3">
+    </label>
+
     <div class="card">
         @foreach ($news as $new)
-            <div class="col-lg-4 col-sm-6 mb-4">
+            <div class="col-lg-4 col-sm-6 mb-2 mt-4">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">{{ $new->title }}</h5>
@@ -18,6 +21,17 @@
                     d'informations</a>
             </div>
         @endforeach
+
+        <div class="" style="text-align: center">
+            <div class="format">
+                {{$news->links('livewire.pagination')}}
+            </div>
+        </div>
     </div>
-    {{$news->links()}}
 </div>
+
+<style>
+    .format {
+        display: inline-block;
+    }
+</style>
