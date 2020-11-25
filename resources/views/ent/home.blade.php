fc307524254199c64ca070014f4986d8eb97326f
Mohamed <mohamed.aarouss@ens.univ-artois.fr>
2020-11-25 17:32:40 +0100

    Footer

--- resources/views/ent/home.blade.php
+++ resources/views/ent/home.blade.php
@@ -287,8 +287,71 @@
     <button class="btn btn-primary" id="save">Save</button>
 
     </body>
-</x-app-layout>
+    <footer class="mainfooter" role="contentinfo">
+        <div class="footer-middle">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-pad">
+                            <h4>Scolarité</h4>
+                            <ul class="list-unstyled">
+                                <li><a href="#">LIEN 1</a></li>
+                                <li><a href="#">LIEN 2</a></li>
+                                <li><a href="#">LIEN 3</a></li>
+                                <li><a href="#">LIEN 4</a></li>
+                                <li><a href="#">LIEN 5</a></li>
+                                <li><a href="#">LIEN 6</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-pad">
+                            <h4>Campus en ligne</h4>
+                            <ul class="list-unstyled">
+                                <li><a href="#">LIEN 1</a></li>
+                                <li><a href="#">LIEN 2</a></li>
+                                <li><a href="#">LIEN 3</a></li>
+                                <li><a href="#">LIEN 4</a></li>
+                                <li><a href="#">LIEN 5</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-pad">
+                            <h4>Bureau virtuel</h4>
+                            <ul class="list-unstyled">
+                                <li><a href="#">LIEN 1</a></li>
+                                <li><a href="#">LIEN 2</a></li>
+                                <li><a href="#">LIEN 3</a></li>
+                                <li><a href="#">LIEN 4</a></li>
+                                <li><a href="#">LIEN 5</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-pad">
+                            <h4>Documentation</h4>
+                            <ul class="list-unstyled">
+                                <li><a href="#">LIEN 1</a></li>
+                                <li><a href="#">LIEN 2</a></li>
+                                <li><a href="#">LIEN 3</a></li>
+                                <li><a href="#">LIEN 4</a></li>
+                                <li><a href="#">LIEN 5</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 copy">
+                        <p class="text-center">&copy; Copyright 2018 - Company Name.  All rights reserved.</p>
+                    </div>
+                </div>
 
+
+            </div>
+        </div>
+    </footer>
+</x-app-layout>
 </html>
 
 <script>
@@ -330,6 +393,35 @@
 </script>
 
 <style>
+    footer {
+        background: #16222A;
+        background: linear-gradient(59deg, #0087A7, #024772);
+        color: white;
+        margin-top:100px;
+    }
+
+    footer a {
+        color: #fff;
+        font-size: 14px;
+        transition-duration: 0.2s;
+    }
+
+    footer a:hover {
+        color: #FA944B;
+        text-decoration: none;
+    }
+
+    .copy {
+        font-size: 12px;
+        padding: 10px;
+        border-top: 1px solid #FFFFFF;
+    }
+
+    .footer-middle {
+        padding-top: 2em;
+        color: white;
+    }
+
     .red {
         background-color: #D2351F;
         border-radius: 5px;
